cac5ba5b723fcd3f2a452b5327dac0f19418cb55
waqarhamza <waqarhamza@gmail.com>
2019-05-29 13:02:44 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a Read Me',
-    owner   => 'root',
-    }
-  }
+}
+node master.puppet.vm {
+  include role:master_server
+}
